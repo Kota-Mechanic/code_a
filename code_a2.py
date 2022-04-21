9631ae707cae27349405be444b15ac953dc7afe1
Kota6543 <84553620+Kota6543@users.noreply.github.com>
2022-04-21 16:45:13 +0900

    Add files via upload

---
+++ code_a2.py
@@ -0,0 +1,19 @@
+# count one in dataType
+
+data_a=[
+    {"dataType": "xurg"},
+    {"dataType": "4by35"},
+    {"dataType": "one"},
+    {"dataType": "44yt52"},
+    {"dataType": "one"},
+    {"dataType": "one"},
+    {"dataType": "4by35"},
+    {"dataType": "one"},
+    {"dataType": "one"}
+]
+
+a=[element for element in data_a if element['dataType'] == "one"]
+
+print(len(a))
+
+# output: 5
