84c7a48f4466f03941d2656fbf3d0e293a1f9f6a
Kota6543 <84553620+Kota6543@users.noreply.github.com>
2022-04-26 20:20:23 +0900

    Add files via upload

---
+++ code_a2.py
@@ -0,0 +1,135 @@
+data = [
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    },
+    {
+      "x": "count_this"
+    }
+]
+
+print(len(data))
+
+#output: 43
